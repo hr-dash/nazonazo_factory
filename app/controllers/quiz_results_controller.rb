bb7f13952d6b586384c0a5abbd4cfe5cbdda2a14
“masataka.ohara” <1989.oxhara@gmail.com>
2016-02-09 21:40:06 +0900

    sprocketバグ修正、クイズ結果の骨組みだけ作成、画面は文字だけ

---
+++ app/controllers/quiz_results_controller.rb
@@ -0,0 +1,7 @@
+class QuizResultsController < ApplicationController
+
+def index
+
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   resources :quiz_solve
 
+  resources :quiz_results
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
